b12074986f33c1da7e306fcfef93f91a56314658
NyImo <yimo22@naver.com>
2021-03-07 15:58:03 +0900

    트리 - DFS/Tree

--- 1068.cpp
+++ 1068.cpp
@@ -4,43 +4,87 @@
 	Coded by yimo22
 */
 #include <iostream>
-#include <vector>
 using namespace std;
 #define Max 50
-typedef struct Node{
+typedef struct Node
+{
     int value;
     int parent_Node;
-}Node;
+} Node;
 Node s[Max];
-void DeleteNode(int rootNode,int size){
-    s[rootNode].parent_Node = -2;
+int Visited[Max];
+void DeleteNode(int dNode, int size)
+{
+    s[dNode].parent_Node = -2;
 
-    for(int i=rootNode+1;i<size;i++){
-        if(s[i].parent_Node == rootNode && s[i].parent_Node != -2){
-            DeleteNode(i,size);
-        } 
+    for (int i = 0; i < size; i++)
+    {
+        if (s[i].parent_Node == dNode && s[i].parent_Node != -2)
+        {
+            DeleteNode(i, size);
+        }
     }
+}
+int Find_Leaf(int size)
+{
+    int cnt = 0, root;
+    // 루트만 남은 경우
+
+    for (; root < size; root++)
+        if (s[root].parent_Node == -1)
+        {
+            break;
+        }
+    for (int i = 0; i < size; i++)
+    {
+        if (s[i].parent_Node == -2) // deleted
+            continue;
+        bool flag = false;
+        if (Visited[i] != 1)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                if (s[i].parent_Node == -2) // deleted
+                    continue;
+
+                if (s[j].parent_Node == i)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                continue;
+            }
+            else
+            {
+                Visited[i] = 1;
+                cnt++;
+            }
+        }
+    }
+
+    return cnt;
 }
 int main()
 {
-    int cnt_node =0;
+    int cnt_node = 0;
     cin >> cnt_node;
-    for(int i=0;i<cnt_node;i++){
+    for (int i = 0; i < cnt_node; i++)
+    {
         Node temp;
         temp.value = i;
-        cin >> temp.parent_Node ;
+        cin >> temp.parent_Node;
         s[i] = temp;
     }
     int del_node;
     cin >> del_node;
 
     // 해당 원소 삭제
-    DeleteNode(del_node,cnt_node);
+    DeleteNode(del_node, cnt_node);
 
-
-    
-
-
+    cout << Find_Leaf(cnt_node);
 
     return 0;
 }
